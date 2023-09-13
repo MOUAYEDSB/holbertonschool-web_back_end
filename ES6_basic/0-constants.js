4ff2a2a9c8e3a2d32df061a341cee05bdc007134
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-09-13 23:47:16 +0100

    const or let

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -2,11 +2,10 @@ export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
-  export function getLast() {
-  return ' is okay';  }
-  export function taskNext() {
+export function getLast() {
+  return ' is okay';00 }
+export function taskNext() {
   let combination = 'But sometimes let';
-	combination += getLast();
+  combination += getLast();
   return combination;
 }
-  
