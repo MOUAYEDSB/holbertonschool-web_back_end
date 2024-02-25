7eded54b67c13872875b8de2a317fd82130169ac
MOUAYEDSB <sabbaghmouayed@gmail.com>
2024-02-25 20:19:17 +0100

    Error handler: Unauthorized

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,26 +1,13 @@
-#!/usr/bin/env python3
-""" Module of Index views
-"""
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+# Existing routes...
+# ...
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
-    """
-    return jsonify({"status": "OK"})
+# New endpoint to raise 401 error
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint():
+    abort(401)
 
-
-@app_views.route('/stats/', strict_slashes=False)
-def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
-    """
-    from models.user import User
-    stats = {}
-    stats['users'] = User.count()
-    return jsonify(stats)
+# Existing routes...
+# ...
