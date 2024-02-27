260ba3d6bd5d97a19f2587b8fdd342a4a1b01692
MOUAYEDSB <sabbaghmouayed@gmail.com>
2024-02-27 13:37:29 +0100

    Find user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -2,8 +2,11 @@
 """DB module
 """
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -19,7 +22,7 @@ class DB:
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
-        
+
     @property
     def _session(self) -> Session:
         """Memoized session object
@@ -30,16 +33,25 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
-
-        Args:
-            email (str): Email of the user
-            hashed_password (str): Hashed password of the user
-
-        Returns:
-            User: The created User object
+        """Save the user to the database
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Takes in arbitrary keyword arguments and
+        returns the first row found in the users
+        """
+        try:
+            result = self._session.query(User).filter_by(**kwargs).first()
+            if result is None:
+                raise NoResultFound
+            return result
+        except NoResultFound:
+            raise NoResultFound("No user found.")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query parameters.")
+
+    
