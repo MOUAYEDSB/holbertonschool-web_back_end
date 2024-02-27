1ec325c42a00b3afdc5119289e51b1c1e71a2ca5
MOUAYEDSB <sabbaghmouayed@gmail.com>
2024-02-27 13:13:49 +0100

    user model

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,13 +1,22 @@
-from sqlalchemy import Column, Integer, String
+#!/usr/bin/env python3
+""" User model Module
+"""
+
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'users'
 
+class User(Base):
+    """ User class that inherits from Base class.
+    """
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.email
